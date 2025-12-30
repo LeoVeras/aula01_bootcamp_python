4568a8f399ee5f5c4377f2ee5c669e6c66612e81
LeoVeras <leoo.sbveras@gmail.com>
2025-12-29 21:33:13 -0300

    adicionando exercicios ao arquivo

--- aula01_bootcamp/main.py
+++ aula01_bootcamp/main.py
@@ -1,1 +1,10 @@
-print("hello world")
+# Aula 01 - Bootcamp Python
+# Exercício: Contar caracteres do nome
+quantidade_caracteres_nome = len(input("Digite seu nome: "))
+print("Seu nome tem", quantidade_caracteres_nome, "caracteres.")
+# Fim do exercício
+
+numero1 = input('Digite o primeiro número: ')
+numero2 = input('Digite o segundo número: ')
+soma = float(numero1) + float(numero2)
+print('A soma dos dois números é:', soma)
